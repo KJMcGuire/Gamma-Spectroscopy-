7f06f59bf5589b1c9822b1efe4d543e1885c4e84
KJMcGuire <34797209+KJMcGuire@users.noreply.github.com>
2018-09-22 19:58:20 -0400

    Improved attenuation fits and added some error analysis

--- absorption_of_radium.py
+++ absorption_of_radium.py
@@ -9,24 +9,30 @@ x0 = np.arange(N)
 x = np.linspace(47, 1887.02, 1011)  #Define Energy space that incorporates linear fit parameters
 
 
-###Subtract global background plus costant from Ra-226 attenuated and unattenuated spectra
-Radium_1mm_lead = data_arrays_take_2.sp3-data_arrays_take_2.sp0 - 100
-Radium = data_arrays_take_2.sp2-data_arrays_take_2.sp0 - 100
-Radium_7mm_lead = data_arrays_take_2.sp3_1-data_arrays_take_2.sp0 - 100
-
-peak = slice(280, 333, 1)
-x1 = np.linspace(281, 333, 53)
-
 ###Define an exponential to fit the background
 def exponential(x, A, b):
     return A*np.exp(-x*b)
+
+###Subtract global and local background Ra-226 attenuated and unattenuated spectra
+Radium_1mm_lead = data_arrays_take_2.sp3-data_arrays_take_2.sp0 - exponential(x0, 3500, .0145) -50
+Radium = data_arrays_take_2.sp2-data_arrays_take_2.sp0 - exponential(x0, 3500, .0145) -50
+Radium_7mm_lead = data_arrays_take_2.sp3_1-data_arrays_take_2.sp0 - exponential(x0, 3500, .0145) -50
+
+peak = slice(265, 330, 1)
+x1 = np.linspace(265, 329, 65)
+
+
 
 ###Define a gaussian curve
 def gaussian(x, sigma, X, A):
     return A*np.exp( (-(x-X)**2)/(2*sigma**2))
 
+
+
+
+
 ###Perform fit of 609 keV peak
-ra_params1, ra_param_covar1 = curve_fit(gaussian, x1, Radium[peak], p0 = [100, 310.,1.])
+ra_params1, ra_param_covar1 = curve_fit(gaussian, x1, Radium[peak], p0 = [90, 310.,1.])
 ra_params2, ra_param_covar2 = curve_fit(gaussian, x1, Radium_1mm_lead[peak], p0 = [100, 310.,1.])
 ra_params3, ra_param_covar3 = curve_fit(gaussian, x1, Radium_7mm_lead[peak], p0 = [100, 310.,1.])
 
@@ -45,19 +51,65 @@ for i in range(peak.start, peak.stop + 1):
 
 print sum1, sum2, sum3
 
+###Errors for curve fits
+perr = np.sqrt(np.diag(abs(ra_params1)))   #Gives 1 standard deviation error of fit parameters
+
+print perr
+
+###Calculate Chi^2 of fits
+numerator = 0
+denominator = 0
+sum = 0
+for i in range(peak.start, peak.stop + 1):
+    numerator = ( Radium[i] - gaussian(x0[i], *ra_params2) )**2
+    denominator = gaussian(x0[i], *ra_params2)
+    sum+= (numerator/denominator)
+print "Chi^2 = {sum}".format(sum=sum)
+
+##################
+#Define test data to check functions
+##################
+
+
+def test_gaussian(x, sigma, X):
+    return 1/(sigma*np.sqrt(2*np.pi))*np.exp( (-(x-X)**2)/(2*sigma**2))
+
+
+
+
+
+###Calculate uncertainty of counts
+def count_uncert(sigma, A, dsigma, dA):
+    delta_c = np.sqrt(  np.pi*2*(sigma**2)*(dA)**2     +  2*np.pi*(A*dsigma)**2    )
+    return delta_c
+
+count_uncertainty = count_uncert(ra_params1[0], ra_params1[2], perr[0,0], perr[2,2])
+print count_uncertainty
+
+#Calculate the uncertainty in lead absorption coefficient
+
+def del_mu(I, I0, x, dI, dI0, dx):
+    np.sqrt( (dI0/(x*I0))**2  + (dI/(x*I))**2   + (( np.log(I0/I) * dx)/(x**2))**2    )
+
+delta_mu_1_25mm = del_mu(sum2, sum1, 1.25, 3888, 4586, 0.5)
+print delta_mu_1_25mm
+
+
 #Plot 609keV photopeaks with fits and count no.
 plt.figure(figsize=(10,7))
 plt.plot(x, Radium, label="Unattenuated", color = '#ff474c')
-plt.plot(x, gaussian(x0, *ra_params1), color = '#ff474c', linestyle = '-.', label="Fit")
-plt.plot(x, Radium_1mm_lead, color = '#2c6fbb', label="1 mm attenuator" )
-plt.plot(x, gaussian(x0,  *ra_params2), color = '#2c6fbb', linestyle = '-.', label="Fit")
-plt.plot(x, Radium_7mm_lead, color = '#fec615', label="7 mm attenuator")
-plt.plot(x, gaussian(x0,  *ra_params3), color = '#fec615', linestyle = '-.', label="Fit")
-plt.annotate('14,316', xy=(609, 490), xytext=(609, 490))
+plt.plot(x, gaussian(x0, *ra_params1), color = '#ff474c', linestyle = '-.')
+plt.plot(x, Radium_1mm_lead, color = '#2c6fbb', label="1.25 mm attenuator" )
+plt.plot(x, gaussian(x0,  *ra_params2), color = '#2c6fbb', linestyle = '-.')
+plt.plot(x, Radium_7mm_lead, color = '#fec615', label="6.7 mm attenuator")
+plt.plot(x, gaussian(x0,  *ra_params3), color = '#fec615', linestyle = '-.')
+plt.annotate('14,316 ', xy=(609, 490), xytext=(609, 490))
 plt.annotate('11,945', xy=(609, 390), xytext=(609, 390))
 plt.annotate('5,614', xy=(609, 190), xytext=(609, 190))
-plt.xlim([549,669])
-plt.ylim([0,1000])
+#plt.xlim([0, 1000])
+plt.xlim([550,675])
+plt.ylim([0,800])
+#plt.title("Lead attenuation of Ra-226 609 keV photopeak")
 plt.xlabel("Energy (keV)")
 plt.ylabel("Count")
 plt.legend(loc=1, prop={'size': 15})
---
+++ error_calculations_take_2.py
@@ -0,0 +1,65 @@
+import calibration_take_2
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sp
+
+#Calculate errors for Channel No. vs. Energy linear fit
+def sigma_y(A, B, x, y):
+    sum = 0.0
+    for i in range( len(y) ):
+            sum += (y[i] - A - B*x[i])**2
+    return np.sqrt( (1/ (len(y)-2.) )  *  sum )
+
+sigma_y = sigma_y(calibration_take_2.b, calibration_take_2.m, calibration_take_2.energy_calibration1[1], calibration_take_2.energies)
+
+#Calculate chi^2 uncertainty
+
+#Calculate uncertainty in parameters m and b in y = mx + b
+def delta(x):
+    sum1 = 0.0
+    sum2 = 0.0
+    for i in range( len(x) ):
+        sum1 += x[i]**2
+        sum2 += (x[i])
+    return len(x)*sum1-(sum2)**2
+
+def sigma_b(sig_y, x, delta):
+    sum = 0.0
+    for i in range (  len(x)):
+        sum += x[i]**2
+    return sig_y* np.sqrt( sum / delta )
+
+def sigma_m(sig_y, x, delta):
+    return sig_y* np.sqrt( len(x) / delta )
+
+delta = delta(calibration_take_2.energy_calibration1[1])
+sigma_b = sigma_b(sigma_y, calibration_take_2.energy_calibration1[1], delta)
+sigma_m = sigma_m(sigma_y, calibration_take_2.energy_calibration1[1], delta)
+
+print sigma_b
+print sigma_m
+#print calibration.m
+
+
+
+
+#Calculate relative resolution of the detector for each photopeak and compute their average/ with uncertainty
+
+#resolutions = np.array( [[len(calibration.energy_calibration1[1])], [len(calibration.energy_calibration1[1])]], np.int32)
+
+def res(sigma, E):
+    res = 0.0
+    sum = 0.0
+    for i in range ( len(calibration_take_2.energy_calibration1[0])):
+        res = (2.*abs(sigma[i])*100.*calibration_take_2.m)/E[i]
+        print res
+#        resolutions.fill(res())
+        sum += res
+    print (sum/ [len(calibration_take_2.energy_calibration1[0])])
+
+
+
+
+
+#resolution = res(calibration.energy_calibration1[0], calibration.energies)
+#print resolution
