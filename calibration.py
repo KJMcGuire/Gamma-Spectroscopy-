b0ecab52cb3f35be860fcc26ee767b8a7d57815c
KJMcGuire <34797209+KJMcGuire@users.noreply.github.com>
2018-09-22 09:14:05 -0400

    Subtracted local background
    Fitted gaussians to clean peaks. This created new sigmas that make more sense.

--- calibration.py
+++ calibration.py
@@ -4,11 +4,6 @@ import numpy as np
 import scipy as sp
 from scipy.optimize import curve_fit
 from scipy.stats import linregress
-#from sympy.interactive import printing
-#printing.init_printing(use_latex=True)
-#from IPython.display import display, Math, Latex
-
-
 
 
 N = 1011
@@ -29,74 +24,76 @@ cesium_s2 = slice(280, 348, 1)
 x2 = np.linspace(281, 348, 68)
 cesium_s3 = slice(347, 1011, 1)
 x3 = np.linspace(348, 1011, 664)
-radium_s1 = slice(257, 314, 1)
-ra_x1 = np.linspace(258, 314, 57)
-radium_s2 = slice(138, 175, 1)
-ra_x2 = np.linspace(139, 175, 37)
-radium_s3 = slice(113, 138, 1)
-ra_x3 = np.linspace(114, 138, 25)
-radium_s4 = slice(90, 112, 1)
-ra_x4 = np.linspace(91, 112, 22)
-radium_s5 = slice(68, 85, 1)
-ra_x5 = np.linspace(69, 85, 17)
-
+radium_s1 = slice(254, 306, 1)
+ra_x1 = np.linspace(255, 306, 52)
+radium_s2 = slice(128, 168, 1)
+ra_x2 = np.linspace(129, 168, 40)
+radium_s3 = slice(99, 134, 1)
+ra_x3 = np.linspace(100, 134, 35)
+radium_s4 = slice(79, 112, 1)
+ra_x4 = np.linspace(80, 112, 33)
+radium_s5 = slice(63, 85, 1)
+ra_x5 = np.linspace(64, 85, 22)
 
 ###Define a gaussian curve plus exponential term
-def gaussian(x, sigma, X, c):
-    return (1/(sigma*np.sqrt(2*np.pi))) * np.exp( (-(x-X)**2)/(2*sigma**2) + c)
+def gaussian(x, sigma, X, A):
+    return A*np.exp( (-(x-X)**2)/(2*sigma**2))
 
-###Perform curve fitting of photopeaks
-ce_params2, ce_param_covars2 = curve_fit(gaussian, x2, Cesium[cesium_s2], p0= [1., 320., 1.])
-ra_params1, ra_param_covar1 = curve_fit(gaussian, ra_x1, Radium[radium_s1], p0 = [210., 340., 1.])
-ra_params2, ra_param_covar2 = curve_fit(gaussian, ra_x2, Radium[radium_s2], p0 = [130., 220., 1.])
-ra_params3, ra_param_covar3 = curve_fit(gaussian, ra_x3, Radium[radium_s3], p0 = [110., 140., 1.])
-ra_params4, ra_param_covar4 = curve_fit(gaussian, ra_x4, Radium[radium_s4], p0 = [90., 115., 1.])
-ra_params5, ra_param_covar5 = curve_fit(gaussian, ra_x5, Radium[radium_s5], p0 = [65., 95., 1.])
-print (ce_params2)
-print (ra_params5)
+###Define an exponential to fit the background
+def exponential(x, A, b):
+    return A*np.exp(-x*b)
 
-###Create lists of sigmas and channel numbers: the List Comprehension Way
-energy_calibration1 = [t for t in zip(ce_params2, ra_params1, ra_params2, ra_params3, ra_params4, ra_params5)[:2]]
+#Subtract local background from known spectra
+Radium_clean = Radium - exponential(x, 3500, .0145)
+Cesium_clean = Cesium - exponential(x, 3500, .0145)
 
 
-#print energy_calibration1
+ce_params1, ce_param_covar1 = curve_fit(gaussian, x2, Cesium[cesium_s2], p0 = [100, 340.,1.])
+ra_params1c, ra_param_covar1c = curve_fit(gaussian, ra_x1, Radium_clean[radium_s1], p0 = [100, 340.,1.])
+ra_params2c, ra_param_covar2c = curve_fit(gaussian, ra_x2, Radium_clean[radium_s2], p0 = [130., 220.,1.])
+ra_params3c, ra_param_covar3c = curve_fit(gaussian, ra_x3, Radium_clean[radium_s3], p0 = [110., 140.,1.])
+ra_params4c, ra_param_covar4c = curve_fit(gaussian, ra_x4, Radium_clean[radium_s4], p0 = [90., 115.,1.])
+ra_params5c, ra_param_covar5c = curve_fit(gaussian, ra_x5, Radium_clean[radium_s5], p0 = [65., 95.,1.])
 
-###The Longer Imperative Way
-energy_calibration2 = []
-times = 0
-for t in zip(ce_params2, ra_params1, ra_params2, ra_params3, ra_params4, ra_params5):
-    times+=1
-    if times > 2:
-        break
-    energy_calibration2.append(t)
-
-###Perform curve fitting of energy versus channel no.
-m, b, r_value, p_value, std_err = linregress(energy_calibration1[1], energies)
-
-###Plot spectra with fits
 plt.figure(figsize=(10,7))
-plt.plot(x, Cesium, linewidth=1, color = '#047495', label='Cs-137')
-plt.plot(x, Radium, linewidth=2, color = '#fac205', linestyle = '--', label='Ra-226')
-plt.plot(x2, gaussian(x2, *ce_params2), color = '#02ab2e',  linestyle = '-.', label='Fit curves', )
-plt.plot(ra_x1, gaussian(ra_x1, *ra_params1), color = '#02ab2e', linestyle = '-.')
+plt.plot(x2, gaussian(x2, *ce_params1), color = '#02ab2e',  linestyle = '-.', label='Fit curves')
+plt.plot(ra_x1, gaussian(ra_x1, *ra_params1c), color = '#02ab2e', linestyle = '-.')
+plt.plot(ra_x2, gaussian(ra_x2, *ra_params2c), color = '#02ab2e', linestyle = '-.')
+plt.plot(ra_x3, gaussian(ra_x3, *ra_params3c), color = '#02ab2e', linestyle = '-.')
+plt.plot(ra_x4, gaussian(ra_x4, *ra_params4c), color = '#02ab2e', linestyle = '-.')
+plt.plot(ra_x5, gaussian(ra_x5, *ra_params5c), color = '#02ab2e', linestyle = '-.')\
+
+#plt.plot(x, exponential(x, 3500, 0.0145))
+
 plt.annotate('662 keV', xy=(320, 820), xytext=(320, 820))
-plt.annotate('609 keV', xy=(260, 870), xytext=(260, 870))
-plt.annotate('352 keV', xy=(155, 1880), xytext=(155, 1880))
-plt.annotate('295 keV', xy=(110, 1680), xytext=(110, 1680))
-plt.annotate('242 keV', xy=(90, 1400), xytext=(90, 1400))
-plt.annotate('197 keV', xy=(50, 1530), xytext=(50, 1530))
-plt.plot(ra_x2, gaussian(ra_x2, *ra_params2), color = '#02ab2e', linestyle = '-.')
-plt.plot(ra_x3, gaussian(ra_x3, *ra_params3), color = '#02ab2e', linestyle = '-.')
-plt.plot(ra_x4, gaussian(ra_x4, *ra_params4), color = '#02ab2e', linestyle = '-.')
-plt.plot(ra_x5, gaussian(ra_x5, *ra_params5), color = '#02ab2e', linestyle = '-.')
-#plt.title("Gaussian fit to photopeaks")
-plt.xlim([0,600])
-plt.ylim([0,2500])
+plt.annotate('609 keV', xy=(260, 820), xytext=(260, 820))
+plt.annotate('352 keV', xy=(150, 1600), xytext=(150, 1600))
+plt.annotate('295 keV', xy=(110, 1280), xytext=(110, 1280))
+plt.annotate('242 keV', xy=(85, 600), xytext=(85, 600))
+plt.annotate('197 keV', xy=(52, 530), xytext=(52, 530))
+plt.plot(x, Radium_clean, linewidth=2, color = '#2c6fbb', linestyle = '--', label='Ra-226')
+plt.plot(x, Cesium_clean, linewidth=1, color = '#ff474c', label='Cs-137')
+plt.xlim([0,400])
+plt.ylim([0, 3000])
 plt.xlabel("Channel No.")
 plt.ylabel("Count")
 plt.legend(loc=1, prop={'size': 15})
-plt.savefig("Gaussian_fit_to_photo_peaks_DRAFT.png", bbox_inches="tight", pad_inches=0)
-#plt.show()
+plt.savefig("Gaussian_fit_to_photo_peaks.png", bbox_inches="tight", pad_inches=0)
+plt.show()
+
+#print ra_params1c
+#print ra_params2c
+#print ra_params3c
+#print ra_params4c
+#print ra_params5c
+
+###Create lists of sigmas and channel numbers: the List Comprehension Way
+energy_calibration1 = [t for t in zip(ce_params1, ra_params1c, ra_params2c, ra_params3c, ra_params4c, ra_params5c)[:2]]
+
+#print energy_calibration1
+
+###Perform curve fitting of energy versus channel no.
+m, b, r_value, p_value, std_err = linregress(energy_calibration1[1], energies)
 
 #Plot linear fit
 x = np.arange(50, 350, 1)
@@ -105,32 +102,33 @@ fig, ax = plt.subplots()
 ax.plot((x), m*(x) + b)
 ax.scatter(energy_calibration1[1], energies, )
 textstr = '\n'.join((
-    r'$m=%.2f  (\sigma_{m}=.02)$' % (m, ),
-    r'$b=%.0f   (\sigma_{b}=3.5)$' % (b, )))
+    r'$m=%.2f  (\sigma_{m}=0.02)$' % (m, ),
+    r'$b=%.0f   (\sigma_{b}=3.8)$' % (b, )))
 props = dict(boxstyle='round', facecolor='gray', alpha=0.5)
 ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=12, verticalalignment='top', bbox=props)
 #plt.title("Channel No. versus Energy")
-B = [312.86, 285.33, 153.22, 125.22, 99.74, 74.90]
+B = [312.86, 285.71, 153.46, 125.73, 100.83, 75.85]
 for xy in zip(B, energies):
     ax.annotate(' (%s, %s keV)' % xy, xy=xy, textcoords='data')
 plt.xlim([0,500])
 plt.ylim([0,800])
 plt.xlabel("Channel No.")
 plt.ylabel("Energy (keV)")
-plt.savefig("Linear_fit_DRAFT.png", bbox_inches="tight", pad_inches=0)
+plt.savefig("Linear_fit.png", bbox_inches="tight", pad_inches=0)
 #plt.show()
+
 
 ###Create new plot with energy on x axis, using linear fit data
 x = np.linspace(44, 2045.78, 1011)  #Define linear space that incorporates linear fit parameters
 plt.figure(figsize=(10,7))
-plt.plot(x, Cesium, linewidth=1, color = '#047495', label='Cs-137')
-plt.plot(x, Radium, linewidth=2, color = '#fac205', linestyle = '--', label='Ra-226')
-plt.plot(x, Unknown, linewidth=1, color = '#ae7181', linestyle = '-.', label='Sample No. 3')
+plt.plot(x, Cesium, linewidth=1, color = '#ff474c', label='Cs-137')
+plt.plot(x, Radium, linewidth=1, color = '#2c6fbb', linestyle = '--', label='Ra-226')
+plt.plot(x, Unknown, linewidth=2, color = '#10a674', linestyle = '-.', label='Sample No. 3')
 plt.xlim([0,2046])
 plt.ylim([0,2500])
 #plt.title('Gamma Spectra (Calibrated)')
 plt.xlabel("Energy (keV)")
 plt.ylabel("Count")
 plt.legend(loc=1, prop={'size': 15})
-plt.savefig("gamma_spectra_calibrated_DRAFT.png", bbox_inches="tight", pad_inches=0)
+plt.savefig("gamma_spectra_calibrated.png", bbox_inches="tight", pad_inches=0)
 #plt.show()
--- error_calculations.py
+++ error_calculations.py
@@ -1,4 +1,5 @@
 import calibration
+#import calibration_take_2
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
@@ -14,7 +15,7 @@ sigma_y = sigma_y(calibration.b, calibration.m, calibration.energy_calibration1[
 
 #Calculate chi^2 uncertainty
 
-#Calculate uncertainty in parameters A and B in y = Bx + A
+#Calculate uncertainty in parameters m and b in y = mx + b
 def delta(x):
     sum1 = 0.0
     sum2 = 0.0
@@ -23,36 +24,41 @@ def delta(x):
         sum2 += (x[i])
     return len(x)*sum1-(sum2)**2
 
-def sigma_A(sig_y, x, delta):
+def sigma_b(sig_y, x, delta):
     sum = 0.0
     for i in range (  len(x)):
         sum += x[i]**2
     return sig_y* np.sqrt( sum / delta )
 
-def sigma_B(sig_y, x, delta):
+def sigma_m(sig_y, x, delta):
     return sig_y* np.sqrt( len(x) / delta )
 
 delta = delta(calibration.energy_calibration1[1])
-sigma_A = sigma_A(sigma_y, calibration.energy_calibration1[1], delta)
-sigma_B = sigma_B(sigma_y, calibration.energy_calibration1[1], delta)
+sigma_b = sigma_b(sigma_y, calibration.energy_calibration1[1], delta)
+sigma_m = sigma_m(sigma_y, calibration.energy_calibration1[1], delta)
 
-#print sigma_A
-#print sigma_B
+print sigma_b
+print sigma_m
 #print calibration.m
 
 
-resolutions = np.array( [[len(calibration.energy_calibration1[1])], [len(calibration.energy_calibration1[1])]], np.int32)
+
 
 #Calculate relative resolution of the detector for each photopeak and compute their average/ with uncertainty
+
+#resolutions = np.array( [[len(calibration.energy_calibration1[1])], [len(calibration.energy_calibration1[1])]], np.int32)
+
 def res(sigma, E):
     res = 0.0
     sum = 0.0
-    for i in range ( len(calibration.energy_calibration1[1])):
-        res =  (2.*sigma[i])*10./E[i]
+    for i in range ( len(calibration.energy_calibration1[0])):
+        res = (2.*abs(sigma[i])*100.*calibration.m)/E[i]
         print res
-        #resolutions.fill(res())
+#        resolutions.fill(res())
         sum += res
-    print (sum/ [len(calibration.energy_calibration1[1])])
+    print (sum/ [len(calibration.energy_calibration1[0])])
 
-resolution = res(calibration.energy_calibration1[1], calibration.energies)
-print resolutions
+
+
+#resolution = res(calibration.energy_calibration1[0], calibration.energies)
+#print resolution
