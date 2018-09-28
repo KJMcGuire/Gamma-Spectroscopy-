a1fe47b57952a6124d2e1d5356150790e83f7ae3
KJMcGuire <34797209+KJMcGuire@users.noreply.github.com>
2018-09-28 07:39:55 -0400

    New attenuation fits, chi-squared, modified plots

--- absorption_of_radium.py
+++ absorption_of_radium.py
@@ -7,6 +7,7 @@ from scipy.optimize import curve_fit
 N = 1011
 x0 = np.arange(N)
 x = np.linspace(47, 1887.02, 1011)  #Define Energy space that incorporates linear fit parameters
+plt.rcParams.update({'font.size': 15})
 
 
 ###Define an exponential to fit the background
@@ -14,27 +15,35 @@ def exponential(x, A, b):
     return A*np.exp(-x*b)
 
 ###Subtract global and local background from Ra-226 attenuated and unattenuated spectra
-Radium_1mm_lead = data_arrays_take_2.sp3-data_arrays_take_2.sp0 - exponential(x0, 3500, .0145) -50
-Radium = data_arrays_take_2.sp2-data_arrays_take_2.sp0 - exponential(x0, 3500, .0145) -50
-Radium_7mm_lead = data_arrays_take_2.sp3_1-data_arrays_take_2.sp0 - exponential(x0, 3500, .0145) -50
+Radium_1mm_lead = data_arrays_take_2.sp3-data_arrays_take_2.sp0
+Radium = data_arrays_take_2.sp2-data_arrays_take_2.sp0
+Radium_7mm_lead = data_arrays_take_2.sp3_1-data_arrays_take_2.sp0
 
-peak = slice(265, 330, 1)
-x1 = np.linspace(265, 329, 65)
+Radium_1mm_lead_clean = Radium_1mm_lead - exponential(x0, 3500, .018) -30
+Radium_clean = Radium  - exponential(x0, 3500, .018) -20
+Radium_7mm_lead_clean = Radium_7mm_lead - exponential(x0, 3500, .018) -30
 
+
+
+###Create data slices for curve fitting and error analysis
+peak = slice(280, 330, 1)
+x1 = np.linspace(280, 330, 50)
+
+Radium_cut = Radium_clean[peak]
+Radium_cut_1mm = Radium_1mm_lead_clean[peak]
+Radium_cut_7mm = Radium_7mm_lead_clean[peak]
+
+#print Radium_cut_7mm
 
 
 ###Define a gaussian curve
 def gaussian(x, sigma, X, A):
     return A*np.exp( (-(x-X)**2)/(2*sigma**2))
 
-
-
-
-
 ###Perform fit of 609 keV peak
-ra_params1, ra_param_covar1 = curve_fit(gaussian, x1, Radium[peak], p0 = [90, 310.,1.])
-ra_params2, ra_param_covar2 = curve_fit(gaussian, x1, Radium_1mm_lead[peak], p0 = [100, 310.,1.])
-ra_params3, ra_param_covar3 = curve_fit(gaussian, x1, Radium_7mm_lead[peak], p0 = [100, 310.,1.])
+ra_params1, ra_param_covar1 = curve_fit(gaussian, x1, Radium_clean[peak], p0 = [90, 310.,1.])
+ra_params2, ra_param_covar2 = curve_fit(gaussian, x1, Radium_1mm_lead_clean[peak], p0 = [100, 310.,1.])
+ra_params3, ra_param_covar3 = curve_fit(gaussian, x1, Radium_7mm_lead_clean[peak], p0 = [100, 310.,1.])
 
 #print ra_params1
 #print ra_params2
@@ -49,24 +58,62 @@ for i in range(peak.start, peak.stop + 1):
     sum2 += gaussian(x0[i], *ra_params2)   #1mm
     sum3 += gaussian(x0[i], *ra_params3)   #7mm
 
-#print sum1, sum2, sum3
+print sum1, sum2, sum3
 
 ###Errors for curve fits
-perr = np.sqrt(np.diag(abs(ra_params3)))   #Gives 1 standard deviation error of fit parameters
+perr_1 = np.sqrt(np.diag(abs(ra_params1)))   #Gives 1 standard deviation error of fit parameters
+perr_2 = np.sqrt(np.diag(abs(ra_params2)))
+perr_3 = np.sqrt(np.diag(abs(ra_params3)))
+print perr_1, perr_2, perr_3
+print ra_params1, ra_params2, ra_params3
 
 #print perr
 
 ###Calculate Chi^2 of fits
+
 numerator = 0
 denominator = 0
 sum = 0
-for i in range(peak.start, peak.stop + 1):
-    numerator = ( Radium[i] - gaussian(x0[i], *ra_params2) )**2
-    denominator = gaussian(x0[i], *ra_params2)
+for i in range( len(Radium_cut_7mm) ):
+    numerator = ( Radium_cut_7mm[i] - gaussian(x1[i], *ra_params3))**2
+    denominator = gaussian(x1[i], *ra_params3 )
     sum+= (numerator/denominator)
-#print "Chi^2 = {sum}".format(sum=sum)
 
 
+Chi_2_7mm = sum
+reduced_Chi_2_7mm = sum/(len(Radium_cut_7mm)-1)
+#print "Chi^2 7mm = {Chi_2_7mm}".format(Chi_2_7mm=Chi_2_7mm)
+#print "Reduced Chi^2 7mm = {reduced_Chi_2_7mm}".format(reduced_Chi_2_7mm=reduced_Chi_2_7mm)
+
+
+
+numerator = 0
+denominator = 0
+sum = 0
+for i in range( len(Radium_cut) ):
+    numerator = ( Radium_cut[i] - gaussian(x1[i], *ra_params1))**2
+    denominator = gaussian(x1[i], *ra_params1 )
+    sum+= (numerator/denominator)
+
+
+
+Chi_2_unattenuated = sum
+reduced_Chi_2_unattenuated = sum/(len(Radium_cut)-1)
+#print "Chi^2 unattenuated = {Chi_2_unattenuated}".format(Chi_2_unattenuated=Chi_2_unattenuated)
+#print "Reduced Chi^2 unattenuated = {reduced_Chi_2_unattenuated}".format(reduced_Chi_2_unattenuated=reduced_Chi_2_unattenuated)
+
+numerator = 0
+denominator = 0
+sum = 0
+for i in range( len(Radium_cut_1mm) ):
+    numerator = ( Radium_cut_1mm[i] - gaussian(x1[i], *ra_params2))**2
+    denominator = gaussian(x1[i], *ra_params2 )
+    sum+= (numerator/denominator)
+
+Chi_2_1mm = sum
+reduced_Chi_2_1mm = sum/(len(Radium_cut_1mm)-1)
+#print "Chi^2 1mm = {Chi_2_1mm}".format(Chi_2_1mm=Chi_2_1mm)
+#print "Reduced Chi^2 1mm = {reduced_Chi_2_1mm}".format(reduced_Chi_2_1mm=reduced_Chi_2_1mm)
 
 
 ###Calculate uncertainty of counts
@@ -74,8 +121,10 @@ def count_uncert(sigma, A, dsigma, dA):
     delta_c = np.sqrt(  np.pi*2*(sigma**2)*(dA)**2     +  2*np.pi*(A*dsigma)**2    )
     return delta_c
 
-count_uncertainty = count_uncert(ra_params3[0], ra_params3[1], perr[0,0], perr[1,1])
-#print count_uncertainty
+count_uncertainty_1 = count_uncert( abs(ra_params1[0]), ra_params1[1], perr_1[0,0], perr_1[1,1])
+count_uncertainty_2 = count_uncert( abs(ra_params2[0]), ra_params2[1], perr_2[0,0], perr_2[1,1])
+count_uncertainty_3 = count_uncert( abs(ra_params3[0]), ra_params3[1], perr_3[0,0], perr_3[1,1])
+print count_uncertainty_1, count_uncertainty_2, count_uncertainty_3
 
 #Calculate the uncertainty in lead absorption coefficient
 
@@ -83,23 +132,24 @@ def del_mu(I, I0, x, dI, dI0, dx):
     np.sqrt( (dI0/(x*I0))**2  + (dI/(x*I))**2   + (( np.log(I0/I) * dx)/(x**2))**2    )
 
 delta_mu_1_25mm = del_mu(sum2, sum1, 1.25, 2570, 2578, 0.5)
-print delta_mu_1_25mm
+#print delta_mu_1_25mm
 
 
 #Plot 609keV photopeaks with fits and count no.
 plt.figure(figsize=(10,7))
-plt.plot(x, Radium, label="Unattenuated", color = '#ff474c')
+#plt.plot(x, exponential(x0, 3500, .018) + 40)
+plt.plot(x, Radium_clean, label="Unattenuated, \tilde{\chi}^2 = 5.1", color = '#ff474c')
 plt.plot(x, gaussian(x0, *ra_params1), color = '#ff474c', linestyle = '-.')
-plt.plot(x, Radium_1mm_lead, color = '#2c6fbb', label="1.25 mm attenuator" )
+plt.plot(x, Radium_1mm_lead_clean, color = '#2c6fbb', label="1.25 mm attenuator, $\tilde{\chi} = 4.0" )
 plt.plot(x, gaussian(x0,  *ra_params2), color = '#2c6fbb', linestyle = '-.')
-plt.plot(x, Radium_7mm_lead, color = '#fec615', label="7 mm attenuator")
+plt.plot(x, Radium_7mm_lead_clean, color = '#fec615', label="7 mm attenuator, \tilde{\chi}^2 = 2.3")
 plt.plot(x, gaussian(x0,  *ra_params3), color = '#fec615', linestyle = '-.')
-plt.annotate('14,697 ', xy=(609, 490), xytext=(609, 490))
-plt.annotate('12,332', xy=(609, 390), xytext=(609, 390))
-plt.annotate('5,966', xy=(609, 190), xytext=(609, 190))
+plt.annotate('17,700 ', xy=(609, 490), xytext=(609, 490))
+plt.annotate('14,800', xy=(609, 390), xytext=(609, 390))
+plt.annotate('8,400', xy=(609, 190), xytext=(609, 190))
 #plt.xlim([0, 1000])
-plt.xlim([550,675])
-plt.ylim([0,800])
+plt.xlim([560,660])
+plt.ylim([-10,800])
 #plt.title("Lead attenuation of Ra-226 609 keV photopeak")
 plt.xlabel("Energy (keV)")
 plt.ylabel("Count")
--- calibration.py
+++ calibration.py
@@ -5,6 +5,7 @@ import scipy as sp
 from scipy.optimize import curve_fit
 from scipy.stats import linregress
 
+plt.rcParams.update({'font.size': 15})
 
 N = 1011
 x = np.arange(N)
@@ -68,43 +69,59 @@ ra_params6c, ra_param_covar6c = curve_fit(gaussian, ra_x6, Radium_clean[radium_s
 unknown_params_1, unknown_covar_1 = curve_fit(gaussian, un_x1, Unknown[unknown_1], p0 = [65., 210,1.])
 unknown_params_2, unknown_covar_2 = curve_fit(gaussian, un_x2, Unknown[unknown_2], p0 = [65., 550,1.])
 
+print ra_params6c
+
+###Errors for curve fits
+err_ce1 = np.sqrt(np.diag(abs(ce_param_covar1c)))   #Gives 1 standard deviation error of fit parameters
+err_ra1 = np.sqrt(np.diag(abs(ra_param_covar1c)))
+err_ra2 = np.sqrt(np.diag(abs(ra_param_covar2c)))
+err_ra3 = np.sqrt(np.diag(abs(ra_param_covar3c)))
+err_ra4 = np.sqrt(np.diag(abs(ra_param_covar4c)))
+err_ra5 = np.sqrt(np.diag(abs(ra_param_covar5c)))
+err_ra6 = np.sqrt(np.diag(abs(ra_param_covar6c)))
+
+#print err_ce1
+
+###Create lists of Gaussian fit errors
+Gaussian_fit_errors = [t for t in zip(err_ra6, err_ce1, err_ra1, err_ra2, err_ra3,err_ra4, err_ra5)[:3]]
+
+print Gaussian_fit_errors
 
 #print ra_params6c
-###Errors for curve fits
-perr = np.sqrt(np.diag(abs(ce_param_covar1c)))   #Gives 1 standard deviation error of fit parameters
+
 
 #print perr
 #print ce_params1c
 
 plt.figure(figsize=(10,7))
 plt.plot(x2, gaussian(x2, *ce_params1c), color = '#2c6fbb',  linestyle = '-.', label='Fit curves')
-#plt.plot(ra_x1, gaussian(ra_x1, *ra_params1c), color = '#2c6fbb', linestyle = '-.')
-#plt.plot(ra_x2, gaussian(ra_x2, *ra_params2c), color = '#2c6fbb', linestyle = '-.')
-#plt.plot(ra_x3, gaussian(ra_x3, *ra_params3c), color = '#2c6fbb', linestyle = '-.')
-#plt.plot(ra_x4, gaussian(ra_x4, *ra_params4c), color = '#2c6fbb', linestyle = '-.')
-#plt.plot(ra_x5, gaussian(ra_x5, *ra_params5c), color = '#2c6fbb', linestyle = '-.')
-#plt.plot(ra_x6, gaussian(ra_x6, *ra_params6c), color = '#2c6fbb', linestyle = '-.')
+plt.plot(ra_x1, gaussian(ra_x1, *ra_params1c), color = '#2c6fbb', linestyle = '-.')
+plt.plot(ra_x2, gaussian(ra_x2, *ra_params2c), color = '#2c6fbb', linestyle = '-.')
+plt.plot(ra_x3, gaussian(ra_x3, *ra_params3c), color = '#2c6fbb', linestyle = '-.')
+plt.plot(ra_x4, gaussian(ra_x4, *ra_params4c), color = '#2c6fbb', linestyle = '-.')
+plt.plot(ra_x5, gaussian(ra_x5, *ra_params5c), color = '#2c6fbb', linestyle = '-.')
+plt.plot(ra_x6, gaussian(ra_x6, *ra_params6c), color = '#2c6fbb', linestyle = '-.')
 
 #plt.plot(un_x1, gaussian(un_x1, *unknown_params_1), color = '#02ab2e', linestyle = '-.')
 #plt.plot(un_x2, gaussian(un_x2, *unknown_params_2), color = '#02ab2e', linestyle = '-.')
 #plt.plot(un_x1, Unknown[unknown_1])
-plt.plot(x, Unknown, linewidth=1)
+#plt.plot(x, Unknown, linewidth=1)
 #plt.plot(x, exponential(x, 3500, 0.0145, 0))
-plt.annotate('1120 keV', xy=(535, 140), xytext=(535, 140))
-plt.annotate('662 keV', xy=(320, 820), xytext=(320, 820))
-plt.annotate('609 keV', xy=(260, 820), xytext=(260, 820))
-plt.annotate('352 keV', xy=(130, 1600), xytext=(130, 1600))
-plt.annotate('295 keV', xy=(95, 1150), xytext=(95, 1150))
-plt.annotate('242 keV', xy=(80, 600), xytext=(80, 600))
-plt.annotate('197 keV', xy=(40, 515), xytext=(40, 515))
-#plt.plot(x, Radium_clean, linewidth=1, color = '#fec615', linestyle = '--', label='Ra-226')
-#plt.plot(x, Cesium_clean, linewidth=1, color = '#ff474c', label='Cs-137')
+plt.annotate('662 keV', xy=(320, 920), xytext=(320, 920))
+plt.annotate('1120 keV', xy=(535, 240), xytext=(535, 240))
+plt.annotate('609 keV', xy=(240, 920), xytext=(240, 920))
+plt.annotate('352 keV', xy=(120, 1580), xytext=(120, 1580))
+plt.annotate('295 keV', xy=(80, 1200), xytext=(80, 1200))
+plt.annotate('242 keV', xy=(50, 700), xytext=(50, 700))
+plt.annotate('197 keV', xy=(30, 490), xytext=(30, 490))
+plt.plot(x, Radium_clean, linewidth=1, color = '#fec615', linestyle = '--', label='Ra-226')
+plt.plot(x, Cesium_clean, linewidth=1, color = '#ff474c', label='Cs-137')
 plt.xlim([0,600])
-plt.ylim([0, 3000])
+plt.ylim([0, 2500])
 plt.xlabel("Channel No.")
 plt.ylabel("Count")
 plt.legend(loc=1, prop={'size': 15})
-#plt.savefig("Gaussian_fit_to_photo_peaks.png", bbox_inches="tight", pad_inches=0)
+plt.savefig("Gaussian_fit_to_photo_peaks_w_unknown.png", bbox_inches="tight", pad_inches=0)
 plt.show()
 
 
@@ -115,6 +132,7 @@ energy_calibration1 = [t for t in zip(ra_params6c, ce_params1c, ra_params1c, ra_
 
 ###Create lists of delta_sigmas from gaussian fits
 sigma_variances = [t for t in zip(ra_param_covar6c, ce_param_covar1c, ra_param_covar1c, ra_param_covar2c, ra_param_covar3c, ra_param_covar4c, ra_param_covar5c)[:1]]
+full_width = [15.75*2, 9.84*2, 9.81*2, 6.20*2, 4.95*2, 3.72*2, 2.67*2]
 
 #print sigma_variances[0][1][0]
 #print ra_param_covar1c[0][0]
@@ -122,25 +140,44 @@ sigma_variances = [t for t in zip(ra_param_covar6c, ce_param_covar1c, ra_param_c
 ###Perform curve fitting of energy versus channel no.
 m, b, r_value, p_value, std_err = linregress(energy_calibration1[1], energies)
 
-#print m
-#print b
+print  "The slope is {m}".format(m=m)
+print  "And the y-int is {b}".format(b=b)
+
+
 #Plot linear fit
 x = np.arange(50, 600, 1)
 #plt.figure(figsize=(10,7))
 fig, ax = plt.subplots()
+#ax.scatter(energy_calibration1[1], energies)
+ax.errorbar(energy_calibration1[1], energies, xerr=(full_width), capsize=5, fmt='o', color = '#2c6fbb' )
 ax.plot((x), m*(x) + b)
-ax.scatter(energy_calibration1[1], energies)
+
+###Calculate Chi^2 of linear fit
+def chi_2_linear(x_data, y_data, slope, intercept):
+    numerator = 0
+    denominator = 0
+    sum = 0
+    for i in range( len(y_data)):
+        numerator = ( y_data[i] - (slope*x_data[i]+intercept))**2
+        denominator = y_data[i]
+        sum+= (numerator/denominator)
+    return sum
+
+chi_2_linear_fit = chi_2_linear(energy_calibration1[1], energies, m, b)
+reduced_chi_2 = chi_2_linear_fit/(len(energies)-1)
+
 #plt.annotate(' keV', xy=(580, 1000), xytext=(580, 1000))
 textstr = '\n'.join((
-    r'$m=%.2f  (\sigma_{m}=0.02)$' % (m, ),
-    r'$b=%.0f   (\sigma_{b}=5.9)$' % (b, )))
+    r'$m=%.2f \pm 0.02$' % (m, ),
+    r'$b=%.0f \pm 6$' % (b, ),
+    r'$\tilde{\chi}^2 =%.2f $' % (reduced_chi_2, )))
 props = dict(boxstyle='round', facecolor='gray', alpha=0.5)
 ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=12, verticalalignment='top', bbox=props)
 #plt.title("Channel No. versus Energy")
 B = [561.24, 313.09, 285.65, 153.45, 125.73, 100.83, 75.85]
-for xy in zip(B, energies):
-    ax.annotate(' %s, %s keV' % xy, xy=xy, textcoords='data')
-plt.xlim([0,700])
+#for xy in zip(B, energies):
+#    ax.annotate(' %s, %s keV' % xy, xy=xy, textcoords='data')
+plt.xlim([0,650])
 plt.ylim([0,1300])
 plt.xlabel("Channel No.")
 plt.ylabel("Energy (keV)")
@@ -152,21 +189,22 @@ Energy2 = unknown_params_2[1]*m+b
 
 #print Energy1, Energy2
 #print unknown_params_1[0], unknown_params_2[0]
-print unknown_params_1[1], unknown_params_2[1]
+print  unknown_params_1[1]
+print  unknown_params_2[1]
 
 ###Create new plot with energy on x axis, using linear fit data
 x = np.linspace(42, 2053.89, 1011)  #Define linear space that incorporates linear fit parameters
 plt.figure(figsize=(10,7))
-plt.plot(x, Unknown_clean, linewidth=1, color = '#ff474c', label='Sample No. 3')
-plt.plot(x, Radium_clean, linewidth=1, color = '#2c6fbb', linestyle = '-.', label='Ra-226')
-plt.plot(x, Cesium_clean, linewidth=1, color = '#fec615', linestyle = '--', label='Cs-137')
-plt.annotate("560keV",  xy=(470, 970))
-plt.annotate("1068keV",  xy=(1000, 350))
-plt.xlim([0,2046])
-plt.ylim([0,1750])
+plt.plot(x, Unknown_clean, linewidth=2, color = '#ff474c', linestyle = '-.', label='Sample No. 3')
+plt.plot(x, Radium_clean, linewidth=1, color = '#2c6fbb', label='Ra-226')
+plt.plot(x, Cesium_clean, linewidth=2, color = '#fec615', linestyle = '--', label='Cs-137')
+plt.annotate("560$\pm$6.0 keV",  xy=(400, 930))
+plt.annotate("1068$\pm$8.7 keV",  xy=(950, 340))
+plt.xlim([0,1500])
+plt.ylim([0,1500])
 #plt.title('Gamma Spectra (Calibrated)')
 plt.xlabel("Energy (keV)")
 plt.ylabel("Count")
 plt.legend(loc=1, prop={'size': 15})
-plt.savefig("gamma_spectra_calibrated.png", bbox_inches="tight", pad_inches=0)
+#plt.savefig("gamma_spectra_calibrated.png", bbox_inches="tight", pad_inches=0)
 plt.show()
